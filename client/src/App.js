9f76bc76a2f0104b5d6ca266eeca19ec3ea5e82f
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-29 13:32:57 +0600

    socket

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:5000");
 
 export default function App() {
-    const sendMessage = ()=>{
-        
-    }
+  const [message, setMessage] = useState("");
+
+  const sendMessage = () => {};
+
   return (
     <div>
       <div>
-        <input type="text" placeholder="message" />
+        <input
+          type="text"
+          placeholder="message"
+          onChange={(e) => setMessage(e.target.value)}
+        />
         <button>Send</button>
       </div>
     </div>
