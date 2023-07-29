83dae2b7c3cdfbe53654fdcfc1ad6e210790d9b7
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-29 13:23:24 +0600

    io

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
-
+const { Server } = require("socket.io");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+
+})
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
